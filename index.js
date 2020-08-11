40d686c9bd338eff984f7e1bd597272e40a3b708
Nusiba Ibrahim <ainusiba@gmail.com>
2020-08-11 20:51:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
+bfast = new Breakfast("eggs", "juice");
+bfast.food;
+bfast.drink;
